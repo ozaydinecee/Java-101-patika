46ebb3a9a06d311b4052f519c2766a98a224acc6
ozaydinecee <ozaydinecee@gmail.com>
2022-11-01 20:11:14 +0300

    area and perimeter of the circle

---
+++ src/CircleAreaandPerimeter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CircleAreaandPerimeter {
+    public static void main(String[] args){
+        double pi=3.14;
+
+        double radius,angle,area,perimeter;
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the radius:");
+        radius=input.nextInt();
+        System.out.println("Enter the angle:");
+        angle=input.nextInt();
+
+        area=(pi*radius*radius* angle) / 360;
+        perimeter=2*pi*radius;
+
+        System.out.println("Area of the circle is "+area);
+        System.out.println("Perimeter of the circle is "+perimeter);
+
+
+
+
+
+    }
+}
