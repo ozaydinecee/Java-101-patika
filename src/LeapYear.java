0404afd4ac94ee4505689e3120f3e9223d7530be
ozaydinecee <ozaydinecee@gmail.com>
2022-11-02 19:22:57 +0300

    project explanation added

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 public class LeapYear {
     public static void main(String[] args) {
         int year;
+        /*
+        * Checks the entered year is divisible by 4, if it s divisible that means it s a leap year.
+        * Years which is divisible by 4 and 100 can divisible with 400 that means this year leap year also.
+        * */
         Scanner input =new Scanner(System.in);
         System.out.println("Enter year:");
         year=input.nextInt();
