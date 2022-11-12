317ddd706661edf07e3aacddc4ed80fe83e687d6
ozaydinecee <ozaydinecee@gmail.com>
2022-11-12 21:17:40 +0300

    number prediction

--- src/NumberPrediction.java
+++ src/NumberPrediction.java
@@ -1,37 +1,57 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class NumberPrediction {
-    public static void main(String[] args){
-        Scanner input =new Scanner(System.in);
-
+    public static void main(String[] args) {
         Random rand = new Random();
         int number = rand.nextInt(100);
+        //int number = (int) (Math.random() * 100);
 
-        int prediction;
-        int right=5;
-        while(right>=5){
-            System.out.println("Please enter your prediction:");
-            prediction=input.nextInt();
+        Scanner input = new Scanner(System.in);
+        int right = 0;
+        int selected;
+        int[] wrong = new int[5];
+        boolean isWin = false;
+        boolean isWrong = false;
 
-            if(prediction<0 || prediction>100){
-                System.out.println("Invalid number.");
+        while (right < 5) {
+            System.out.print("Lütfen tahmininizi giriniz : ");
+            selected = input.nextInt();
 
+            if (selected < 0 || selected > 99) {
+                System.out.println("Lütfen 0-100 arasında bir değer giriniz.");
+                if (isWrong) {
+                    right++;
+                    System.out.println("Çok fazla hatalı giriş yaptınız. Kalan hak : " + (5 - right));
+                } else {
+                    isWrong = true;
+                    System.out.println("Bir daha hatalı girişinizde hakkınızdan düşülecektir.");
+                }
+                continue;
             }
-            else if(prediction==number){
-                System.out.println("Congratulations! Your prediction is true.");
+
+            if (selected == number) {
+                System.out.println("Tebrikler, doğru tahmin ! Tahmin ettiğini sayı : " + number);
+                isWin = true;
                 break;
+            } else {
+                System.out.println("Hatalı bir sayı girdiniz !");
+                if (selected > number) {
+                    System.out.println(selected + " sayısı, gizli sayıdan büyüktür.");
+                } else {
+                    System.out.println(selected + " sayısı, gizli sayıdan küçüktür.");
+                }
+
+                wrong[right++] = selected;
+                System.out.println("Kalan hakkı : " + (5 - right));
             }
-            else{
-                System.out.println("You couldn't find it!");
-                if(prediction>number){
-                    System.out.println(prediction + " is bigger than number.");
+        }
 
-                }
-                else{
-                    System.out.println(prediction + " is smaller than number.");
-
-                }
+        if (!isWin) {
+            System.out.println("Kaybettiniz ! Doğru sayı:"+ number);
+            if (!isWrong) {
+                System.out.println("Tahminleriniz : " + Arrays.toString(wrong));
             }
         }
 
